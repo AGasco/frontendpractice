947bbbef6ad46f288e50ecf634ad78dfcf199775
Antonio Gasco <antoniofgasco@gmail.com>
2021-10-11 13:10:21 +0100

    Adding head and footer to layout

--- components/Layout/Layout.component.js
+++ components/Layout/Layout.component.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Head from '../Head/Head.component';
 import Footer from '../Footer/Footer.component';
 
 function LayoutComponent({ children }) {
   return (
     <>
       {/* TODO: Add Navbar and Footer components */}
+      <Head />
       <div className="navbar" />
       <main>{children}</main>
       <Footer />
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,5 @@
-import HeadComponent from '../components/Head.component';
-import Footer from '../components/Footer/Footer.component';
 import HomeMain from '../components/HomeMain/HomeMain.component';
 
 export default function Home() {
-  return (
-    <>
-      <HomeMain />
-    </>
-  );
+  return <HomeMain />;
 }
