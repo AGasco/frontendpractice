467feb03a8fb2d98e216068371c47a747fe85b5f
Antonio Gasco <antoniofgasco@gmail.com>
2021-10-20 09:13:35 +0100

    Navbar finished

--- components/Ableton/Navbar/Navbar.component.js
+++ components/Ableton/Navbar/Navbar.component.js
@@ -4,26 +4,32 @@ import styles from './Navbar.module.scss';
 
 export const NavbarComponent = () => {
   return (
-    <nav className={styles.navbar}>
-      <div className={styles.navbar__logo}>
-        <Logo />
+    <>
+      <nav className={styles.navbar}>
+        <div className={styles.navbar__logo}>
+          <Logo />
+        </div>
+        <ul className={styles.navbar__left}>
+          <li>Live</li>
+          <li>Push</li>
+          <li>Link</li>
+          <li>Shop</li>
+          <li>Packs</li>
+          <li>Help</li>
+          <li className={`${styles.More} ${styles.Orange}`}>
+            More <span>+</span>
+          </li>
+          <li className={styles.navbar__right}>
+            <div className={styles.TryLive}>Try Live for free</div>
+            <div className={styles.Login}>Log in or register</div>
+          </li>
+        </ul>
+      </nav>
+      <div className={styles.subNavbar}>
+        <div className={styles.Orange}>About</div>
+        <div>Jobs</div>
       </div>
-      <ul className={styles.navbar__left}>
-        <li>Live</li>
-        <li>Push</li>
-        <li>Link</li>
-        <li>Shop</li>
-        <li>Packs</li>
-        <li>Help</li>
-        <li className={styles.More}>
-          More <span>+</span>
-        </li>
-        <li className={styles.navbar__right}>
-          <div className={styles.TryLive}>Try Live for free</div>
-          <div className={styles.Login}>Log in or register</div>
-        </li>
-      </ul>
-    </nav>
+    </>
   );
 };
 
